26fb6119130d3ec980dba035b7de9938bb411880
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-14 11:47:22 +0200

    feat: header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'Activity bookings';
-
   getYear(): number {
     return new Date().getFullYear();
   }
