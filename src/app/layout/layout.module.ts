9d4217ea424a066582f7f3dc4b548ee2860022e3
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-21 11:58:15 +0200

    feat: routes components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CopyRightComponent } from './copy-right/copy-right.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, CopyRightComponent, TitleComponent],
-  imports: [CommonModule],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    CopyRightComponent,
+    TitleComponent,
+  ],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ActivityItemComponent } from './activity-item/activity-item.component';
 import { ListHeaderComponent } from './list-header/list-header.component';
 
-
-
 @NgModule({
-  declarations: [
-    ActivityItemComponent,
-    ListHeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    ActivityItemComponent,
-    ListHeaderComponent
-  ]
+  declarations: [ActivityItemComponent, ListHeaderComponent],
+  imports: [CommonModule],
+  exports: [ActivityItemComponent, ListHeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
